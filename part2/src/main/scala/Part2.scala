f3ff69959787f0198d60cea89609a24f482dbb2b
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2014-10-19 11:56:41 +0100

    done part2

--- part2/src/main/scala/Part2.scala
+++ part2/src/main/scala/Part2.scala
@@ -1,31 +1,29 @@
 object Part2 extends App {
 
-  trait Shape {
-    // body goes here
-  }
+    trait Shape {
+        def width: Double
+        def height: Double
+        def area: Double
+    }
+
+    case class Rectangle(width: Double, height: Double) extends Shape {
+        val area = width * height
+    }
+
+    case class Circle(radius: Double) extends Shape {
+        val width = 2 * radius
+        val height = 2 * radius
+        val area = math.Pi * radius * radius
+    }
 
 
+    val r = Rectangle(5, 8)
+    val c = Circle(10)
 
+    println("r is " + r.width + " units by " + r.height + " units")
+    println("c is " + c.width + " units by " + c.height + " units")
 
-
-
-
-
-
-
-  // Tests for exercise 1
-  // --------------------
-
-  // val r = Rectangle(5, 8)
-  // val c = Circle(10)
-
-  // println("r is " + r.width + " units by " + r.height + " units")
-  // println("c is " + c.width + " units by " + c.height + " units")
-
-  // Tests for exercise 2
-  // --------------------
-
-  // println("The area of r is " + Area.calculate(r))
-  // println("The area of c is " + Area.calculate(c))
+    println("The area of r is " + r.area)
+    println("The area of c is " + c.area)
 
 }
