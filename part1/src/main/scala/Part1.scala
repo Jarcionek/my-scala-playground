17b00688a3426a08549c3b594bf6fcaa5e964c4f
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2014-10-19 11:50:33 +0100

    done part1

--- part1/src/main/scala/Part1.scala
+++ part1/src/main/scala/Part1.scala
@@ -1,7 +1,21 @@
 object Part1 extends App {
 
-  // Write your code here...
+    case class Cat(color: String, favouriteFood: String)
 
-  println("Tip: This is how you print stuff!")
+    val oswald = Cat("black", "milk")
+    val henderson = Cat("ginger and white", "chips")
+    val quentin = Cat("tabby and white", "curry")
+
+    object ChipShop {
+        def serve(cat: Cat): String = cat match {
+            case Cat(_, "chips") => "OMNOM"
+            case _ => "Blehhh"
+        }
+    }
+
+    println("oswald = " + oswald)
+    println("henderson.color = " + henderson.color)
+    println("ChipShop.serve(quentin) = " + ChipShop.serve(quentin))
+    println("ChipShop.serve(henderson) = " + ChipShop.serve(henderson))
 
 }
