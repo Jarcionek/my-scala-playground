8e05a177279f477d0ee1c88e4cbfbeae4fea835f
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2014-10-19 12:34:30 +0100

    done part4

--- part4/src/main/scala/Part4.scala
+++ part4/src/main/scala/Part4.scala
@@ -1,34 +1,52 @@
 object Part4 extends App {
 
-  trait Calculation {
-    def calculate: Int
-  }
+    trait Result
+    case class Finite(value: Int) extends Result
+    case object Undefined extends Result
+
+    
+    trait Calculation {
+        def a: Calculation
+        def b: Calculation
+
+        def calculate: Result = {
+            a.calculate match {
+                case Finite(x) => b.calculate match {
+                    case Finite(y) => op(x, y)
+                    case Undefined => Undefined
+                }
+                case Undefined => Undefined
+            }
+        }
+
+        protected def op(x: Int, y: Int): Result
+    }
+
+    case class Number(value: Int) extends Calculation {
+        val a = null
+        val b = null
+        override val calculate = Finite(value)
+        def op(x: Int, y: Int) = null
+    }
+
+    case class Addition(a: Calculation, b: Calculation) extends Calculation {
+        def op(x: Int, y: Int) = Finite(x + y)
+    }
+
+    case class Multiplication(a: Calculation, b: Calculation) extends Calculation {
+        def op(x: Int, y: Int) = Finite(x * y)
+    }
+
+    case class Division(a: Calculation, b: Calculation) extends Calculation {
+        def op(x: Int, y: Int) = if (y == 0) Undefined else Finite(x / y)
+    }
 
 
 
+    val one = Addition(Number(1), Multiplication(Number(2), Number(3)))
+    val two = Division(Number(1), Number(0))
 
-
-
-
-
-
-
-  // Tests for exercise 1
-  // --------------------
-
-  // val a = Addition(Number(1), Multiplication(Number(2), Number(3)))
-  // val a = Division(Number(1), Number(0))
-
-  // println("a is " + a.calculate)
-  // println("b is " + b.calculate)
-
-  // Additional code for exercise 2
-  // ------------------------------
-
-  // Move this to the top of the file when you get here:
-
-  // trait Result
-  // case class Finite(value: Int) extends Result
-  // case object Undefined extends Result
+    println("one is " + one.calculate)
+    println("two is " + two.calculate)
 
 }
